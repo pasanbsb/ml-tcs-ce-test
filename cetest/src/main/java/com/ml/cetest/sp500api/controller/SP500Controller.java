663c6be2c7526b7f0fc8637cc6f1943b4fe62eb3
Pasan Bandara <pasanbsb@gmail.com>
2025-05-05 13:57:11 -0400

    Added SP500 API call from St Louise FRED

--- cetest/src/main/java/com/ml/cetest/sp500api/controller/SP500Controller.java
+++ cetest/src/main/java/com/ml/cetest/sp500api/controller/SP500Controller.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api")
