44f4dd85a3cd7e8c80c4319c68584977fc713528
Pasan Bandara <pasanbsb@gmail.com>
2025-05-05 13:56:11 -0400

    Added SP500 API call from St Louise FRED

---
+++ cetest/src/main/java/com/ml/cetest/CETestApplication.java
@@ -0,0 +1,23 @@
+package com.ml.cetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class CETestApplication {
+	private static final Logger logger = LoggerFactory.getLogger(CETestApplication.class);
+
+	public static void main(String[] args) {
+		logger.info("Starting ML-TCS CE Test Application");
+		SpringApplication.run(CETestApplication.class, args);
+	}
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}
---
+++ cetest/src/main/java/com/ml/cetest/sp500api/controller/SP500Controller.java
@@ -0,0 +1,35 @@
+package com.ml.cetest.sp500api.controller;
+
+import com.ml.cetest.sp500api.service.SP500Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class SP500Controller {
+
+    private static final Logger logger = LoggerFactory.getLogger(SP500Controller.class);
+
+    private final SP500Service service;
+
+    public SP500Controller(SP500Service service) {
+        this.service = service;
+    }
+
+    // Endpoint to get the latest S&P 500 price
+    @GetMapping("/lastprice")
+    public ResponseEntity<?> getLastPrice() {
+        logger.info("Fetching last S&P 500 price");
+        return ResponseEntity.ok(service.getLastPrice());
+    }
+
+    // Endpoint to get the last 1-month history
+    @GetMapping("/history")
+    public ResponseEntity<?> getOneMonthHistory() {
+        logger.info("Fetching 1-month history of S&P 500 prices");
+        return ResponseEntity.ok(service.getOneMonthHistory());
+    }
+}
