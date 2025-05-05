7f4fb4b2f8baa33fd136b5c52550c1868d688080
Pasan Bandara <pasanbsb@gmail.com>
2025-05-05 12:27:36 -0400

    Initial project generated from Spring Initializr

---
+++ cetest/src/test/java/com/ml/cetest/CetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ml.cetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CetestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
